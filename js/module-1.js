becfa28421a8febef50144dc6e6533c046e87c36
Vadym Chutur <falloutgreck@gmail.com>
2023-04-25 14:34:56 +0300

    update module-4-1

---
+++ js/module-1.js
@@ -0,0 +1,42 @@
+// // Колбек-функція
+// function greet(name) {
+//   console.log(`Ласкаво просимо ${name}.`);
+// }
+
+// // Функція вищого порядку
+// function registerGuest(name, callback) {
+//   console.log(`Реєструємо гостя ${name}.`);
+//   callback(name);
+// }
+
+// registerGuest('Манго', greet);
+
+function processCall(recipient, onAvailable, onNotAvailable) {
+  // Імітуємо доступність абонента випадковим числом
+  const isRecipientAvailable = Math.random() > 0.5;
+
+  if (!isRecipientAvailable) {
+    onNotAvailable(recipient);
+    return;
+  }
+
+  onAvailable(recipient);
+}
+
+function takeCall(name) {
+  console.log(`З'єднуємо з ${name}, очікуйте...`);
+  // Логіка прийняття дзвінка
+}
+
+function activateAnsweringMachine(name) {
+  console.log(`Абонент ${name} недоступний, залиште повідомлення.`);
+  // Логіка активації автовідповідача
+}
+
+function leaveHoloMessage(name) {
+  console.log(`Абонент ${name} недоступний, записуємо голограму.`);
+  // Логіка запису голограми
+}
+
+processCall('Манго', takeCall, activateAnsweringMachine);
+processCall('Полі', takeCall, leaveHoloMessage);
